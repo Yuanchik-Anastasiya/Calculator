f5c816783f84889ac648a8e109faadd0997df037
Yuanchik <zlovcistomvide@gmail.com>
2025-12-09 18:23:59 +0800

    desing2 + function button

--- app/src/main/java/com/yuanchik/calculator/MainActivity.java
+++ app/src/main/java/com/yuanchik/calculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.yuanchik.calculator;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,37 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editText = findViewById(R.id.editText);
+    }
+
+    public void clickNumber(View view) {
+
+        String number = editText.getText().toString();
+
+        if (view.getId() == R.id.bu0) {
+            number = number + "0";
+        } else if (view.getId() == R.id.bu1) {
+            number = number + "1";
+        }else if (view.getId() == R.id.bu2) {
+            number = number + "2";
+        }else if (view.getId() == R.id.bu3) {
+            number = number + "3";
+        }else if (view.getId() == R.id.bu4) {
+            number = number + "4";
+        }else if (view.getId() == R.id.bu5) {
+            number = number + "5";
+        }else if (view.getId() == R.id.bu6) {
+            number = number + "6";
+        }else if (view.getId() == R.id.bu7) {
+            number = number + "7";
+        }else if (view.getId() == R.id.bu8) {
+            number = number + "8";
+        }else if (view.getId() == R.id.bu9) {
+            number = number + "9";
+        }else if (view.getId() == R.id.buAC){
+            number = "0";
+        }
+        editText.setText(number);
     }
 }
